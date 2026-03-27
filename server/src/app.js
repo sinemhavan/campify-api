95559d067e53d4ce15eea336fc22039517423a91
Sinem Havan <github-hesabini-actigin-havansinem@gmail.com>
2026-03-27 21:59:49 +0300

    CORS izni eklendi

--- server/src/app.js
+++ server/src/app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const routes = require('./routesIndex');
+const cors = require('cors');
 
 const app = express();
 
 // Gelen isteklerdeki JSON verilerini okuyabilmek için gerekli middleware
 app.use(express.json());
+app.use(cors());
 
 // OpenAPI tasarımına uygun olarak tüm API rotalarımızı '/api' altına koyuyoruz
 // Yani senin rotan otomatik olarak '/api/posts' haline gelecek
